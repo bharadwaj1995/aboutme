3b8a49c5882854afaaab6fa6cb710df65ae9d1fd
bharadwaj1995 <46728675+bharadwaj1995@users.noreply.github.com>
2019-02-22 16:51:24 -0600

    adding qunit html

---
+++ qunit.js
@@ -0,0 +1,20 @@
+function additontwonumbers(str,str1) {
+            
+    const result = parseInt(str);
+    
+    const result1 = parseInt(str1);
+
+    return result + result1;
+    
+    
+    
+    }
+
+QUnit.test("additontwonumbers", function(assert) {
+	assert.equal(additontwonumbers(30,30), 60, "The addition of two numbers of 30-30 is 60");
+	assert.equal(additontwonumbers(20, 20), 40, "The addition of two numbers of 20-20 is 40");
+	assert.equal(additontwonumbers(10, 10), 20, "The addition of two numbers of 10-10 is 20");
+    assert.equal(additontwonumbers(5, 5), 10, "The addition of two numbers of 5-5 is 10");
+    assert.equal(additontwonumbers(35, 35), 70, "The addition of two numbers of 35-35 is 70");
+	
+ });
